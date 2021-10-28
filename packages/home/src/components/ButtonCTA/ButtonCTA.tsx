2fb529425816d2cf01e8d7a77aceda4867c1985d
Mona <42650066+littlemonastar@users.noreply.github.com>
2021-10-28 11:37:46 -0700

    feat: :art: add new images

--- packages/home/src/components/ButtonCTA/ButtonCTA.tsx
+++ packages/home/src/components/ButtonCTA/ButtonCTA.tsx
@@ -8,7 +8,7 @@ export const ButtonCTA: FC = () => {
       <div className="inline-flex space-x-6">
         <button
           disabled
-          className="inline-flex justify-center items-center py-2 px-4 text-sm font-medium leading-6 text-center text-trueGray-50 disabled:hover:bg-gray-300 bg-gradient-to-r from-pink-400 hover:from-pink-300 disabled:hover:from-pink-200 disabled:from-pink-200 via-purple-400 hover:via-purple-300 disabled:hover:via-purple-200 to-indigo-500 hover:to-indigo-400 disabled:hover:to-indigo-300 disabled:to-indigo-300 rounded-md border hover:border-2 disabled:hover:border border-gray-600 shadow-xl disabled:hover:opacity-60 disabled:opacity-60 hover:scale-105 disabled:hover:scale-100"
+          className="py-2 px-4 font-mono bg-gray-200 hover:bg-gray-300 border-b-2 border-purple-500 hover:border-purple-600"
         >
           Coming soon...
         </button>
@@ -16,7 +16,7 @@ export const ButtonCTA: FC = () => {
           target="_blank"
           rel="noopener noreferrer"
           href={MIRROR_ARTICLE_URL}
-          className="inline-flex justify-center items-center py-2 px-4 text-sm font-medium leading-6 text-center text-pink-400 bg-pink-100 hover:bg-pink-200 rounded-md border border-transparent hover:border-red-400 focus:ring-pink-500"
+          className="py-2 px-4 font-mono bg-gray-200 hover:bg-gray-300 border-b-2 border-purple-500 hover:border-purple-600"
         >
           Learn More
         </a>
--- packages/home/src/components/VaporGrid/VaporGrid.tsx
+++ packages/home/src/components/VaporGrid/VaporGrid.tsx
@@ -5,7 +5,7 @@ import { useRef } from "react";
 import * as THREE from "three";
 
 export const VaporGrid = () => {
-  const SIZE = 100;
+  const SIZE = 150;
   const SEGMENTS = 60;
   const gridRef = useRef<THREE.GridHelper>();
 
@@ -16,12 +16,12 @@ export const VaporGrid = () => {
   return (
     <gridHelper
       ref={gridRef}
-      position={[0, -3, 0]}
+      position={[0, -2, 0]}
       args={[
         SIZE,
         SEGMENTS,
-        new THREE.Color("rgb(217, 70, 239)"),
-        new THREE.Color("rgb(217, 70, 239)"),
+        new THREE.Color("rgb(255, 255, 255)"),
+        new THREE.Color("rgb(255, 255, 255)"),
       ]}
     />
   );
