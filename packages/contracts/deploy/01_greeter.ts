2e5aa339b7192384a13d4d6a2bbcb0cb0a078b2f
Shun Kakinoki <shunkakinoki@gmail.com>
2021-11-06 18:29:46 -0400

    chore: update

--- packages/contracts/deploy/01_greeter.ts
+++ packages/contracts/deploy/01_greeter.ts
@@ -1,16 +1,25 @@
 import type { HardhatRuntimeEnvironment } from "hardhat/types";
 
+import type { Greeter } from "@/typechain/Greeter";
+import type { Greeter__factory } from "@/typechain/factories/Greeter__factory";
+
 const deploy = async ({
   getNamedAccounts,
   deployments,
+  ethers,
 }: HardhatRuntimeEnvironment) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  await deploy("Greeter", {
-    args: ["Hello, world!"],
+  await deploy("MyNFT", {
+    args: [],
     from: deployer,
     log: true,
   });
+  const greeterFactory = (await ethers.getContractFactory(
+    "Greeter",
+  )) as Greeter__factory;
+  const greeter: Greeter = <Greeter>await greeterFactory.deploy("Hello");
+  await greeter.deployed();
 };
 
 deploy.tags = ["Greeter"];
